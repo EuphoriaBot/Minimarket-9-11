00670fd5a7aea068e1d7e2f4b2fe471fcfe4424e
EuphoriaBot <dimasfirja80@gmail.com>
2026-05-03 15:57:19 +0800

    First Commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,11 @@
+public abstract class  Person {
+    private String id;
+    private String nama;
+    private String telepon;
+
+    public Person(String id, String nama, String telepon) {
+        this.id = id;
+        this.nama = nama;
+        this.telepon = telepon;
+    }
+}

e1f70b7cedef9d73d1df8ca18b33aed735f552e5
EuphoriaBot <dimasfirja80@gmail.com>
2026-05-03 16:13:59 +0800

    Test

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,10 +2,12 @@ public abstract class  Person {
     private String id;
     private String nama;
     private String telepon;
+    private String test;
 
     public Person(String id, String nama, String telepon) {
         this.id = id;
         this.nama = nama;
         this.telepon = telepon;
+        this.test = test;
     }
 }

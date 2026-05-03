e2435508e681bc6c6b6d25de94528bafd85681b8
EuphoriaBot <dimasfirja80@gmail.com>
2026-05-03 16:24:15 +0800

    Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,12 +2,47 @@ public abstract class  Person {
     private String id;
     private String nama;
     private String telepon;
-    private String test;
+
+    public Person() {}
 
     public Person(String id, String nama, String telepon) {
         this.id = id;
         this.nama = nama;
         this.telepon = telepon;
-        this.test = test;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean setId(String id) {
+        if (id == null || id.trim().isEmpty()) {
+            System.out.println("ID tidak boleh kosong");
+            return false;
+        }
+        this.id = id;
+        return true;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public boolean setNama(String nama) {
+        if (nama == null || nama.trim().isEmpty()) {
+            System.out.println("Nama tidak boleh kosong");
+            return false;
+        }
+        this.nama = nama;
+        return true;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public boolean setTelepon(String telepon) {
+        this.telepon = telepon;
+        return true;
     }
 }

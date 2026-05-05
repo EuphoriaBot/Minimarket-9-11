a7c5ca78cf439a6b494ec1adf5d016eb5be8a8f7
EuphoriaBot <dimasfirja80@gmail.com>
2026-05-05 16:20:26 +0800

    Supplier.java

---
+++ src/main/java/com/kel4/minimarket911/Supplier.java
@@ -0,0 +1,40 @@
+package com.kel4.minimarket911;
+
+public class Supplier extends Person {
+    private String namaPerusahaan;
+    private String kota;
+
+    public Supplier() {}
+
+    public Supplier(String id, String nama, String telepon, String namaPerusahaan, String kota) {
+        super(id, nama, telepon);
+        this.namaPerusahaan = namaPerusahaan;
+        this.kota = kota;
+    }
+
+    public String getNamaPerusahaan() {
+        return namaPerusahaan;
+    }
+
+    public void setNamaPerusahaan(String namaPerusahaan) {
+        this.namaPerusahaan = namaPerusahaan;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    @Override
+    public String getRingkasan() {
+        return namaPerusahaan + "Kota: " + kota + "glop: " + getNama();
+    }
+
+    @Override
+    public String toString() {
+        return namaPerusahaan;
+    }
+}
